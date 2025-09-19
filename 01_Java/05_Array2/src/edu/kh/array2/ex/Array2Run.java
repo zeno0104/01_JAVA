ca5079e16df7d9ecc0cbaa69673a6ab5ff3f9cb6
zeno0104 <jaehun4086@naver.com>
2025-09-19 16:09:02 +0900

    배열 예제

---
+++ 01_Java/05_Array2/src/edu/kh/array2/ex/Array2Run.java
@@ -0,0 +1,13 @@
+package edu.kh.array2.ex;
+
+public class Array2Run {
+	public static void main(String[] args) {
+		Array2Example array2Example = new Array2Example();
+
+//		array2Example.ex1();
+//		array2Example.ex2();
+		array2Example.ex3();
+		System.out.println("소스트리 테스트");
+
+	}
+}
