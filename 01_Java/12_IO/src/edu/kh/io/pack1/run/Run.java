d5cdd364a4ccd4866df1ebf4b544275d4dbb50ac
zeno0104 <jaehun4086@naver.com>
2025-10-14 12:37:09 +0900

    [Update] 12_IO

---
+++ 01_Java/12_IO/src/edu/kh/io/pack1/run/Run.java
@@ -0,0 +1,12 @@
+package edu.kh.io.pack1.run;
+
+import edu.kh.io.pack1.model.service.FileService;
+
+public class Run {
+	public static void main(String[] args) {
+		FileService fileService = new FileService();
+
+		// fileService.method1();
+		fileService.method2();
+	}
+}
